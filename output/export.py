c3efb8eba4a5167459e10716975d9b785a4f7cd3
Jordy <jordy.l.nav@gmail.com>
2026-06-17 01:30:43 +0100

    fix:corrigi a path para o export

--- output/export.py
+++ output/export.py
@@ -21,7 +21,7 @@ class Export:
             self.expor_tp[i][1] = self.point_list[i][1]
             self.expor_tp[i][2] = self.point_list[i][2]
             self.expor_tp[i][3] = -1 if i < self.vector_list[0] else self.v if i < self.vector_list[1] + self.vector_list[0] else 0
-        np.savetxt(f"dowloads/,{name})", self.expor_tp, fmt="%.6f")
+        np.savetxt(f"output/downloads/{name}", self.expor_tp, fmt="%.6f")
 
     def exportElementList(self,name="elementos.txt"):
         for i in range(len(self.element_list)):
@@ -30,9 +30,9 @@ class Export:
             self.export_tetra[i, 2] = self.element_list[i][3]
             self.export_tetra[i, 3] = self.element_list[i][4]
             self.export_tetra[i, 4] = self.rho
-        np.savetxt(f"dowloads/,{name})", self.export_tetra, fmt="%d")
+        np.savetxt(f"output/downloads/{name}", self.export_tetra, fmt="%.6f")
     def exportVector(self,name="Vetor.txt"):
-        np.savetxt(f"dowloads/,{name})",  self.vector_list, fmt="%d")
+        np.savetxt(f"output/downloads/{name}",  self.vector_list, fmt="%d")
 
     def exportAll(self,name_p="pontos.txt",name_e="elementos.txt",name_v="Vetor.txt"):
         self.exportPointList(name_p)
